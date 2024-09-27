7e21f93588b414eee98b94634bfe66ad39bbabfd
Chermann-KING <codes.smiling@gmail.com>
2024-09-27 23:06:18 +0200

    💄 Main: Components > Add FinanceChart

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,5 +1,6 @@
 import AttendanceChart from "@/components/AttendanceChart";
 import CountChart from "@/components/CountChart";
+import FinanceChart from "@/components/FinanceChart";
 import UserCard from "@/components/UserCard";
 import React from "react";
 
@@ -27,7 +28,9 @@ const AdminPage = () => {
           </div>
         </div>
         {/* BOTTOM CHARTs */}
-        <div className=""></div>
+        <div className="w-full h-[500px]">
+          <FinanceChart />
+        </div>
       </div>
       {/* RIFHT */}
       <div className="w-full lg:w-1/3 bg-green-300">r</div>
---
+++ src/components/FinanceChart.tsx
@@ -0,0 +1,136 @@
+"use client";
+
+import React from "react";
+import Image from "next/image";
+
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
+
+const data = [
+  {
+    name: "Jan",
+    income: 4000,
+    expense: 2400,
+  },
+  {
+    name: "Feb",
+    income: 3000,
+    expense: 1398,
+  },
+  {
+    name: "Mar",
+    income: 2000,
+    expense: 9800,
+  },
+  {
+    name: "Apr",
+    income: 2780,
+    expense: 3908,
+  },
+  {
+    name: "May",
+    income: 1890,
+    expense: 4800,
+  },
+  {
+    name: "Jun",
+    income: 2390,
+    expense: 3800,
+  },
+  {
+    name: "Jul",
+    income: 3490,
+    expense: 4300,
+  },
+  {
+    name: "Aug",
+    income: 3490,
+    expense: 4300,
+  },
+  {
+    name: "Sep",
+    income: 3490,
+    expense: 4300,
+  },
+  {
+    name: "Oct",
+    income: 3490,
+    expense: 4300,
+  },
+  {
+    name: "Nov",
+    income: 3490,
+    expense: 4300,
+  },
+  {
+    name: "Dec",
+    income: 3490,
+    expense: 4300,
+  },
+];
+
+const FinanceChart = () => {
+  return (
+    <div className="flex flex-col bg-white rounded-xl w-full h-full p-4">
+      {/* TITLE */}
+      <div className="flex justify-between items-center">
+        <h2 className="text-lg font-semibold">Finance</h2>
+        <Image
+          src={"/images/moreDark.png"}
+          alt="more icon"
+          width={20}
+          height={20}
+        />
+      </div>
+      {/* CHART */}
+      <ResponsiveContainer width="100%" height="90%">
+        <LineChart width={500} height={300} data={data}>
+          <CartesianGrid strokeDasharray="3 3" stroke="#DDD" />
+          <XAxis
+            dataKey="name"
+            axisLine={false}
+            tick={{ fill: "#D1D5DB" }}
+            tickLine={false}
+            tickMargin={10}
+          />
+          <YAxis
+            axisLine={false}
+            tick={{ fill: "#D1D5DB" }}
+            tickLine={false}
+            tickMargin={20}
+          />
+          <Tooltip
+            contentStyle={{ borderRadius: "10px", borderColor: "lightgray" }}
+          />
+          <Legend
+            align="center"
+            verticalAlign="top"
+            wrapperStyle={{ paddingTop: "10px", paddingBottom: "30px" }}
+          />
+          <Line
+            type="monotone"
+            dataKey="income"
+            stroke="#C3EBFA"
+            strokeWidth={5}
+          />
+          <Line
+            type="monotone"
+            dataKey="expense"
+            stroke="#CFCEFF"
+            strokeWidth={5}
+          />
+        </LineChart>
+      </ResponsiveContainer>
+    </div>
+  );
+};
+
+export default FinanceChart;
