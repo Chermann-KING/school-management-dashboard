981301a806f4678655eb6fd606f2b79756508a7a
Chermann-KING <codes.smiling@gmail.com>
2024-09-27 15:18:04 +0200

    🚧 Main: Add Dasbord pages

---
+++ src/app/(dashboard)/student/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SudentPage = () => {
+  return <div>SudentPage</div>;
+};
+
+export default SudentPage;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        Menu{children}
       </body>
     </html>
   );
