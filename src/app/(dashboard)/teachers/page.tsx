b74b08a9f0c1814250b56d43d75c0270583e8307
Chermann-KING <codes.smiling@gmail.com>
2024-09-28 18:36:46 +0200

    💄 Main: App > Dashboard > Teachers > Add BigCalendar, EventCalendar and Announcements

--- src/app/(dashboard)/teachers/page.tsx
+++ src/app/(dashboard)/teachers/page.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+import Announcements from "@/components/Announcements";
+import BigCalendar from "@/components/BigCalendar";
+// import EventCalendar from "@/components/EventCalendar";
 
 const TeacherPage = () => {
-  return <div>TeacherPage</div>;
+  return (
+    <div className="flex-1 p-4 flex gap-4 flex-col xl:flex-row">
+      {/* LEFT */}
+      <div className="w-full xl:w-2/3">
+        <div className="h-full  min-h-[880px] bg-white p-4 rounded-md">
+          <h2 className="text-lg font-semibold mb-4">Schedule</h2>
+          <BigCalendar />
+        </div>
+      </div>
+      {/* RIGHT */}
+      <div className="w-full xl:w-1/3 flex flex-col gap-8">
+        {/* <EventCalendar /> */}
+        <Announcements />
+      </div>
+    </div>
+  );
 };
 
 export default TeacherPage;
