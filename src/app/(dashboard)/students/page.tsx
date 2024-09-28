235a04e6dd75685573d3b938d17e9f893468619a
Chermann-KING <codes.smiling@gmail.com>
2024-09-28 18:34:24 +0200

    💄 Main: App > Dashboard > Students > Add a min-height

--- src/app/(dashboard)/students/page.tsx
+++ src/app/(dashboard)/students/page.tsx
@@ -7,7 +7,7 @@ const StudentPage = () => {
     <div className="p-4 flex gap-4 flex-col xl:flex-row">
       {/* LEFT */}
       <div className="w-full xl:w-2/3">
-        <div className="h-full bg-white p-4 rounded-md">
+        <div className="h-full min-h-[880px] bg-white p-4 rounded-md">
           <h2 className="text-lg font-semibold my-4">Schedule (4A)</h2>
           <BigCalendar />
         </div>
